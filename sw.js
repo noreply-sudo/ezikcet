5a9e68a2b8d5cfe507930527a382207277bdd263
noreply-sudo <noreply@ezinurse.com>
2026-01-17 07:39:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,54 @@
-const CACHE_NAME = 'ezikcet-v2'; // bump version!
-const ASSETS = [
-  '/EziKCET/',           // ← if your repo is github.com/you/EziKCET
-  '/EziKCET/index.html',
-  '/EziKCET/manifest.json',
-  '/EziKCET/icon-192.png',
-  '/EziKCET/icon-512.png'
+const CACHE_NAME = 'kcet-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/biology.html',
+  '/biologytest.html',
+  '/chemistry.html',
+  '/chemistrytest.html',
+  '/maths.html',
+  '/mathtest.html',
+  '/physics.html',
+  '/physicstest.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // Add any other assets like CSS/JS if you extract them later
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
