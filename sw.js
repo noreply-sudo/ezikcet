22bea70a15c219e97b81f168c0557a14342f47e3
noreply-sudo <noreply@ezinurse.com>
2026-01-17 06:56:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'ezikcet-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
+const CACHE_NAME = 'ezikcet-v2'; // bump version!
+const ASSETS = [
+  '/EziKCET/',           // ← if your repo is github.com/you/EziKCET
+  '/EziKCET/index.html',
+  '/EziKCET/manifest.json',
+  '/EziKCET/icon-192.png',
+  '/EziKCET/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
